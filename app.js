f63b161b73290c7bbc11a7f44e9615650d37ec74
Vikram Shyamvir Chaudhary <94777175+Dychord@users.noreply.github.com>
2024-04-09 23:18:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const press = document.querySelector('.pressed')
+window.addEventListener('keydown',(e)=>{
+    press.innerHTML = `
+    <table>
+    <tr>
+      <th>key</th>
+      <th>keyCode</th>
+      <th>code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td>
+      <td>${e.code}</td>
+    </tr>
+  </table>
+    `
+})
